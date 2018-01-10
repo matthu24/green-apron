775dc550b685a7c2b153a11cd1b124f3e2868709
Matthew Hu <matthu24@gmail.com>
2018-01-10 11:02:28 -0800

    Package json

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 class Splash extends React.Component {
   constructor(props){
     super(props);
@@ -15,14 +15,12 @@ class Splash extends React.Component {
       <div>
       <div className="splash-container">
 
-          <div className="splash-barrier">Cook away.  Your recipes are safe with us.</div>
+          <div className="splash-barrier"></div>
           <img className='splash-image' src="https://s3.us-east-2.amazonaws.com/greenapron/splash_image3.png"/>
           <div className="splash-text">Food is better when it starts from scratch.</div>
       </div>
       <div className="transition">
-        <ReactCSSTransitionGroup transitionName="anim" transitionAppear={true} transitionAppearTimeout={5000} transitionEnter={false} transitionLeave={false}>
-            <h2 className="green-apron">{'Green Apron'}</h2>
-        </ReactCSSTransitionGroup>
+
       </div>
       <div className="testimonials">
         <div className = "testimony1">
@@ -47,3 +45,7 @@ class Splash extends React.Component {
 //
 
 export default Splash;
+//
+// <ReactCSSTransitionGroup transitionName="anim" transitionAppear={true} transitionAppearTimeout={5000} transitionEnter={false} transitionLeave={false}>
+//     <h2 className="green-apron">{'Green Apron'}</h2>
+// </ReactCSSTransitionGroup>
