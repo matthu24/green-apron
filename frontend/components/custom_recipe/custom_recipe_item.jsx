d0ab9277fb18a1a1e7ef068bf68b7f236f4dc830
Matthew Hu <matthu24@gmail.com>
2018-02-10 14:49:30 -0800

    Add styling to custom item

--- frontend/components/custom_recipe/custom_recipe_item.jsx
+++ frontend/components/custom_recipe/custom_recipe_item.jsx
@@ -5,10 +5,10 @@ export default props => {
   return(
     <li className='custom-item'>
       <Link to={`/customRecipes/${props.recipe.id}`}>
+      <div><img className='generic-food' src='https://s3.us-east-2.amazonaws.com/greenapron/genericfood.png'/></div>
       <div>{props.recipe.title}</div>
       </Link>
       <div>{props.recipe.description}</div>
-
     </li>
   )
 }
