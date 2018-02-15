024bfdc605bb3becd3347c8dee04c1494e7142d0
Matthew Hu <matthu24@gmail.com>
2018-02-15 00:39:41 -0800

    Rearrange seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,12 @@ CustomRecipe.destroy_all
 u1 = User.create!(username: "Guest", password: "password")
 
 r1=Recipe.create!(title: "Vegetable Fried Rice", description: "with Torgarashi Peanuts",image_file_name:"https://s3.us-east-2.amazonaws.com/greenapron/Fried+Rice.png",ingredients:"Jasmine rice, Carrots, Baby bok choy, Broccoli, Eggs, Hoisin Sauce")
+r5=Recipe.create!(title: "Chicken and Orange-Kale Salad", description: "with Spicy Tahini Dressing",image_file_name:"https://s3.us-east-2.amazonaws.com/greenapron/Chicken+and+Orange-Kale+Salad.png",ingredients:"Boneless Chicken, Radishes, Kale, Avocado")
+r6=Recipe.create!(title: "Orange-Glazed Meatballs", description: "with Bok Choy & Jasmine Rice",image_file_name:"https://s3.us-east-2.amazonaws.com/greenapron/Orange+Glazed+Meatballs.png",ingredients:"Ground Beef, Baby Bok Choy, Garlic, Carrots, Jasmine Rice")
 r2=Recipe.create!(title: "General Tso's Chicken", description: "with Bok Choy and Jasmine Rice",image_file_name:"https://s3.us-east-2.amazonaws.com/greenapron/General+Tso's.png", ingredients: "Chicken breast, Bok choy, Jasmine rice, Garlic, Rice vinegar")
 r3 = Recipe.create!(title: "Mushroom and Goat Cheese Quiche", description: "with Green Leaf Lettuce Salad", image_file_name:"https://s3.us-east-2.amazonaws.com/greenapron/Quiche.png", ingredients: "Garlic, Lemon, Eggs, Mushrooms, Lettuce, Pie crusts")
 r4=Recipe.create!(title: "Seared Salmon", description: "with Roasted Red Onion Vinaigrette",image_file_name:"https://s3.us-east-2.amazonaws.com/greenapron/Salmon.png",ingredients:"Salmon fillet,Dried dates,Carrots,Red Onion, Cabbage")
-r5=Recipe.create!(title: "Chicken and Orange-Kale Salad", description: "with Spicy Tahini Dressing",image_file_name:"https://s3.us-east-2.amazonaws.com/greenapron/Chicken+and+Orange-Kale+Salad.png",ingredients:"Boneless Chicken, Radishes, Kale, Avocado")
-r6=Recipe.create!(title: "Orange-Glazed Meatballs", description: "with Bok Choy & Jasmine Rice",image_file_name:"https://s3.us-east-2.amazonaws.com/greenapron/Orange+Glazed+Meatballs.png",ingredients:"Ground Beef, Baby Bok Choy, Garlic, Carrots, Jasmine Rice")
+
 
 
 ri1 = RecipeItem.create!(recipe_id: r1.id, item_title:"Prepare and roast the broccoli", item_description: "Remove the honey from the refrigerator to bring to room temperature. Place an oven rack in the center of the oven, then preheat to 450°F. Wash and dry the fresh produce. Cut off and discard the bottom 1/2 inch of the broccoli stem; cut the broccoli into small pieces, keeping the florets intact. Place on a sheet pan. Drizzle with olive oil and season with salt, pepper, and half the togarashi seasoning; toss to coat. Arrange in an even layer. Roast 15 to 17 minutes, or until browned and tender when pierced with a fork. Remove from the oven.", item_image_file_name: "https://s3.us-east-2.amazonaws.com/greenapron/FriedRice1.png")
